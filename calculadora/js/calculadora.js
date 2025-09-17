2d26e66a1096532cf866fda80d9d887d0ad264eb
Carlos <lawlercarlospatricio@gmail.com>
2025-09-17 12:47:05 -0300

    mejora calculadora

--- calculadora/js/calculadora.js
+++ calculadora/js/calculadora.js
@@ -7,33 +7,57 @@
 
 function sumar(){
     resultado=document.getElementById("resultado")
-    numero1=parseFloat(document.getElementById("numero1").value)
-    numero2=parseFloat(document.getElementById("numero2").value)
-    resultado.value=numero1+numero2
+    if( parseFloat(document.getElementById("numero1").value)==document.getElementById("numero1").value && 
+        parseFloat(document.getElementById("numero2").value)==document.getElementById("numero2").value
+      ){
+        numero1=parseFloat(document.getElementById("numero1").value)
+        numero2=parseFloat(document.getElementById("numero2").value)
+        resultado.value=numero1+numero2
+    }else{
+        resultado.value="Error de ingreso de datos"
+    }
     hablar(resultado.value)
 }
 
 function restar(){
     resultado=document.getElementById("resultado")
-    numero1=parseFloat(document.getElementById("numero1").value)
-    numero2=parseFloat(document.getElementById("numero2").value)
-    resultado.value=numero1-numero2
+    if( parseFloat(document.getElementById("numero1").value)==document.getElementById("numero1").value && 
+        parseFloat(document.getElementById("numero2").value)==document.getElementById("numero2").value
+      ){
+        numero1=parseFloat(document.getElementById("numero1").value)
+        numero2=parseFloat(document.getElementById("numero2").value)
+        resultado.value=numero1-numero2
+    }else{
+        resultado.value="Error de ingreso de datos"
+    }
     hablar(resultado.value)
 }
 
 function multi(){
     resultado=document.getElementById("resultado")
-    numero1=parseFloat(document.getElementById("numero1").value)
-    numero2=parseFloat(document.getElementById("numero2").value)
-    resultado.value=numero1*numero2
+    if( parseFloat(document.getElementById("numero1").value)==document.getElementById("numero1").value && 
+        parseFloat(document.getElementById("numero2").value)==document.getElementById("numero2").value
+      ){
+        numero1=parseFloat(document.getElementById("numero1").value)
+        numero2=parseFloat(document.getElementById("numero2").value)
+        resultado.value=numero1*numero2
+    }else{
+        resultado.value="Error de ingreso de datos"
+    }
     hablar(resultado.value)
 }
 
 function divi(){
     resultado=document.getElementById("resultado")
-    numero1=parseFloat(document.getElementById("numero1").value)
-    numero2=parseFloat(document.getElementById("numero2").value)
-    if(numero2!=0)  resultado.value=numero1/numero2
-    else            resultado.value="Error / 0 !"
+    if( parseFloat(document.getElementById("numero1").value)==document.getElementById("numero1").value && 
+        parseFloat(document.getElementById("numero2").value)==document.getElementById("numero2").value
+      ){
+        numero1=parseFloat(document.getElementById("numero1").value)
+        numero2=parseFloat(document.getElementById("numero2").value)
+        if(numero2!=0)  resultado.value=numero1/numero2
+        else            resultado.value="Error / 0 !"
+    }else{
+        resultado.value="Error de ingreso de datos"
+    }
     hablar(resultado.value)
 }
