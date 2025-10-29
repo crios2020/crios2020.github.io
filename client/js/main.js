81136e673e3be7466f9259b974cfefa92866c7c9
Carlos <lawlercarlospatricio@gmail.com>
2025-10-29 20:23:06 -0300

    Client IA

--- client/js/main.js
+++ client/js/main.js
@@ -3,13 +3,12 @@ const apiKeyInput = document.getElementById('api-key');
 const userInput = document.getElementById('user-input');
 const sendButton = document.getElementById('send-button');
 const chatWindow = document.getElementById('chat-window');
-//const URL='https://api.openai.com/v1/chat/completions'
-const URL='https://api.groq.com/openai/v1/chat/completions'
+const URL = 'https://api.groq.com/openai/v1/chat/completions'
 let apiKey = '';
 
 apiKeyInput.addEventListener('input', (e) => {
-    apiKey = e.target.value.trim();
-    sendButton.disabled = !apiKey;
+    //apiKey = e.target.value.trim();
+    //sendButton.disabled = !apiKey;
 });
 
 sendButton.addEventListener('click', sendMessage);
@@ -32,10 +31,10 @@ async function sendMessage() {
     const message = userInput.value.trim();
     if (!message) return;
 
-    addMessage(message, true);
+    addMessage("yo - "+message, true);
     userInput.value = '';
     sendButton.disabled = true;
-
+    apiKey=document.getElementById("api-key").value.trim()
     try {
         const response = await fetch(URL, {
             method: 'POST',
@@ -58,10 +57,25 @@ async function sendMessage() {
 
         const data = await response.json();
         const aiReply = data.choices[0].message.content.trim();
-        addMessage(aiReply, false);
+        addMessage("chatbot - "+aiReply, false);
     } catch (error) {
         addMessage('Error: ' + error.message, false);
     } finally {
         sendButton.disabled = false;
     }
+}
+
+function cargar() {
+    //console.log("Se cargaron elementos..")
+    const claveReversa = [
+        'K', 'N', 'L', 'y', '5', 'A', 'D', 'm', 'L', 'X', 'e', 'S', 'R', 'd', 'g',
+        'c', 'b', 'B', '6', 'B', 'Q', 'v', 'I', 'I', 'Y', 'F', '3', 'b', 'y', 'd', 'G', 
+        'W', 'u', 'W', 'z', '0', 'l', '6', 'g', 'q', 'g', 'I', '5', 'f', 'v', 'C', 'b', 
+        'w', '0', '2', 'B', 'E', '_', 'k', 's', 'g'
+    ];
+    clave="";
+    for(a=claveReversa.length-1; a>=0; a--){
+        clave+=claveReversa[a]
+    }
+    document.getElementById("api-key").value=clave
 }
