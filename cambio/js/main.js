3d857bc5dfbe3af0067414e9f4c6f31ee8d90f6e
carlos <lawlercarlospatricio@gmail.com>
2025-11-07 21:59:04 -0300

    Mejoras cambio

--- cambio/js/main.js
+++ cambio/js/main.js
@@ -6,3 +6,59 @@ function cambiarPesosADolares(cantidadDePesos, cotizacion){
     return cantidadDePesos/cotizacion;
 }
 
+function calcular(){
+    cargar()
+    let monto = parseFloat(document.getElementById("monto").value)
+    let operacion = document.getElementById("operacion").value
+    let dolarCompra=parseFloat(getDolarBlueCompra())
+    let dolarVenta=parseFloat(getDolarBlueVenta())
+
+    //console.log(monto)
+    //console.log(operacion)
+
+    if(operacion=="pesos"){
+        document
+                .getElementById("resultado")
+                .value=cambiarPesosADolares(monto,dolarVenta)
+    }
+    if(operacion=="dolar"){
+        document
+                .getElementById("resultado")
+                .value=cambiarDolaresAPesos(monto,dolarCompra)
+    }
+}
+
+function getDolarBlueCompra(){
+    try {
+        const request = new XMLHttpRequest();
+        request.open("GET", "https://dolarapi.com/v1/dolares/blue", false); 
+        request.send();
+        if (request.status === 200) {
+            const json = JSON.parse(request.responseText)
+            return json.compra
+        }
+    } catch (error) {
+        return "error"
+    }
+}
+
+function getDolarBlueVenta(){
+    try {
+        const request = new XMLHttpRequest();
+        request.open("GET", "https://dolarapi.com/v1/dolares/blue", false); 
+        request.send();
+        if (request.status === 200) {
+            const json = JSON.parse(request.responseText)
+            return json.venta
+        }
+    } catch (error) {
+        return "error"
+    }
+}
+
+function cargar(){
+    //console.log(getDolarBlueCompra())
+    //console.log(getDolarBlueVenta())
+    document.getElementById("compra").value=getDolarBlueCompra()
+    document.getElementById("venta").value=getDolarBlueVenta()
+}
